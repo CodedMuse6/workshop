8c7dc296883d54464865d8f2da0e8a77410e7067
bhawana pal <135514660+bhavanapal@users.noreply.github.com>
2025-11-30 15:31:21 +0000

    half update

--- client/src/Context/AuthContext.ts
+++ client/src/Context/AuthContext.ts
@@ -1,5 +1,5 @@
 import { createContext , useContext } from "react";
-import { type User } from "firebase/auth";
+// import { type User } from "firebase/auth";
 
 // extend firebase's User type to optionally include a role
 // export type AppUser = User & {role?: string};
